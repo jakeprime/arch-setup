d5fef9dd8f979b8091152fc3847afec8c2114fdc
Jake Prime <jake@jakeprime.com>
2025-04-30 09:16:12 +0100

    Fix path typo

--- jake-arch-install-4.sh
+++ jake-arch-install-4.sh
@@ -123,7 +123,7 @@ if [[ $gdrive_sync == [Yy] || -z "$gdrive_sync" ]];
 then
     read -p "Visit https://console.cloud.google.com/apis/api/drive.googleapis.com/credentials?project=g-drive-share-411112 to get the credentials.\nPress any key to continue"
     rclone config
-    mkdir /jake/home/gdrive
+    mkdir /home/jake/gdrive
     echo "Syncing with dry run..."
     rclone bisync gdrive: /home/jake/gdrive --create-empty-src-dirs --compare size,modtime,checksum --slow-hash-sync-only --resilient -MvP --drive-export-formats link.html --fix-case --resync --dry-run
     read -p "Sync for real? (Y/n): " sync_for_real
