053d8671306d7606c62f503e724b4b659d62aa70
Jake Prime <jake@jakeprime.com>
2025-08-30 01:54:56 +0100

    Add monaspace nerd fonts

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo pacman -S --needed --noconfirm awesome-terminal-fonts nerd-fonts noto-fonts noto-fonts-emoji powerline-fonts
+sudo pacman -S --needed --noconfirm awesome-terminal-fonts otf-monaspace-nerd nerd-fonts noto-fonts noto-fonts-emoji powerline-fonts
