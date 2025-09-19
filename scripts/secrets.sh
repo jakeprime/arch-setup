6000136418111b1d416b5fac7a7e080245743bc6
Jake Prime <jake@jakeprime.com>
2025-09-20 00:49:51 +0100

    Load all secrets from Lastpass
    And then store them locally using `pass` for instant access when required.

--- scripts/secrets.sh
+++ scripts/secrets.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-export LPASS_AGENT_TIMEOUT=0
-lpass login "$(git config get user.email)"
+if ! lpass status; then
+  export LPASS_AGENT_TIMEOUT=0
+  lpass login "$(git config get user.email)"
 
-echo "$(lpass show --notes .authinfo)" > $HOME/.authinfo
+  echo "$(lpass show --notes .authinfo)" >$HOME/.authinfo
+fi
 
-# TODO: set up `pass` here too, and import individual secrets
+if ! pass; then
+  echo "$(lpass show gpg/jake@jakeprime.com --field="Public Key")" | gpg --import
+  echo "$(lpass show gpg/jake@jakeprime.com --field="Private Key")" | gpg --import
+  # set to ultimate trust level (5)
+  printf 'trust\n5\ny\nsave\n' |
+    gpg --batch --yes --no-tty --command-fd 0 --status-fd 2 --edit-key jake@jakeprime.com
+
+  mkdir $HOME/.password-store
+  cd $HOME/.password-store
+  git clone git@github.com:jakeprime/secrets .
+fi
