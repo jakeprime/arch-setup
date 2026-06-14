5c3d34ec696f7f61c8890043c55634a2e9cdcd27
Jake Prime <jake@jakeprime.com>
2026-06-14 17:06:02 +0100

    Update email for OAuth2 method
    Needed for Cleo email.

--- scripts/email.sh
+++ scripts/email.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-MAIL_DIR=$HOME/.mail
+yay -S --needed --noconfirm cyrus-sasl-xoauth2-git
+mise x python@3 -- pip3 install --user mutt-oauth2
+
+MAIL_DIR=$XDG_DATA_HOME/mail
 
 mkdir -p $MAIL_DIR/personal
 mkdir -p $MAIL_DIR/cleo
@@ -14,4 +17,7 @@ mu index
 
 # this is going to take tiiiime, but it can be interrupted and resumed at any
 # point, so just kick it off in a fork now
-mbsync --all >/dev/null &
+mbsync cleo >/dev/null &
+
+# Later we'll need to do Cleo with:
+#  mutt-oauth2 --authorize --username jake@meetcleo.com
