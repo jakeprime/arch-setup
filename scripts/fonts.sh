0e08733e045e398e035bc973d3880b4182d48607
Jake Prime <jake@jakeprime.com>
2026-09-23 23:57:32 +0100

    Add fonts for Emacs

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-sudo pacman -S --needed --noconfirm awesome-terminal-fonts otf-monaspace-nerd nerd-fonts noto-fonts noto-fonts-emoji powerline-fonts
+sudo pacman -S --needed --noconfirm \
+     awesome-terminal-fonts \
+     fontconfig \
+     otf-monaspace-nerd \
+     nerd-fonts \
+     noto-fonts \
+     noto-fonts-emoji \
+     powerline-fonts
+
+mkdir -p ~/.local/share/fonts
+curl -L https://raw.githubusercontent.com/jakeprime/arch-setup/main/fonts/SourceSans3-VariableFont_wght.ttf > ~/.local/share/fonts/SourceSans3-VariableFont_wght.ttf
+curl -L https://raw.githubusercontent.com/jakeprime/arch-setup/main/fonts/SourceSans3-Italic-VariableFont_wght.ttf > ~/.local/share/fonts/SourceSans3-Italic-VariableFont_wght.ttf
+curl -L https://raw.githubusercontent.com/jakeprime/arch-setup/main/fonts/Baumans-Regular.ttf > ~/.local/share/fonts/Baumans-Regular.ttf
+fc-cache -f -v
